6861da9f8852afcb08e895ecfed4f18c6c78d21c
yuberk98 <yusufberk98@gmail.com>
2022-10-13 22:56:57 +0300

    Second Patch

--- app_server/controllers/main.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-
-router.get('/',);
-
-module.exports.index=function(req,res,next) {
-    res.render('index' , { title: 'Express' })};
